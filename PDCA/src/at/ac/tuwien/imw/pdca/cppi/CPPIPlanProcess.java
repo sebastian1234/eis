16f242d45a063de7fe9a39c7f35c624402aff1d2
scheickl <s.scheickl@gmx.net>
2014-05-17 17:28:29 +0200

    fertig

--- PDCA/src/at/ac/tuwien/imw/pdca/cppi/CPPIPlanProcess.java
+++ PDCA/src/at/ac/tuwien/imw/pdca/cppi/CPPIPlanProcess.java
@@ -2,13 +2,14 @@ package at.ac.tuwien.imw.pdca.cppi;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.math.BigDecimal;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import at.ac.tuwien.imw.pdca.PlanProcess;
 
-public class CPPIPlanProcess extends PlanProcess implements Closeable {
+public class CPPIPlanProcess extends PlanProcess<BigDecimal> implements Closeable {
 
 	private final static Logger log = LogManager.getLogger(PlanProcess.class);
 	private boolean running = true;
